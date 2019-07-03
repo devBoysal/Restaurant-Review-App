9d0eba0b2e7004061e7249c48e8e6e632cf7af86
Ömer Boysal <oboysal@gmail.com>
2019-07-03 23:33:44 +1000

    Add files via upload
    fixed alt tags for images
    fixed aria labels in index.html

--- js/main.js
+++ js/main.js
@@ -147,6 +147,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'image of restaurant '+ restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
